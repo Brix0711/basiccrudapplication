cf46f5f86104bc4e917caeac5e9760e2badeb9c9
Brix0711 <baitajay25@gmail.com>
2022-10-03 14:10:42 +0800

    First Commit
    First Date:10-03-2022

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>Student Laravel 9 CRUD</title>
+    <meta content="width=device-width, initial-scale=1,maximum-scale=1, user-scalable=n0" name="viewport">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+
+</head>
+<body>
+	@include("partials.navbar")
+
+	@yield('content')
+
+	@include("partials.footer")
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://mxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+  
+</body>
+</html>
